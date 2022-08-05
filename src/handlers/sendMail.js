ceb6a458e36a289c8ea9ecb90323ad359bb16615
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-08-05 11:05:15 +0700

    @58. Sending Our First (Test) Email

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,32 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "us-east-1" });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: "vqkhanh2009@gmail.com",
+    Destination: {
+      ToAddresses: ["vqkhanh2009@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello from khanh",
+        },
+      },
+      Subject: {
+        Data: "Test mail",
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const handler = sendMail;
